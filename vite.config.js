5a3c4e6c3a61e93bb1e367cbc8e90b9853cfc83e
Sahan Dhanujaya <sahandhanujaya18@gmail.com>
2026-07-22 23:48:55 +0530

    faet: ass allowhost

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    allowedHosts: 'lecture-ci-cd-with-github-actions-qzvv.onrender.com',
+  },
 })
